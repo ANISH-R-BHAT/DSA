f4813efdadc8294b6e31966b3a8a3cc68d6f7a86
ANISH-R-BHAT <136636056+ANISH-R-BHAT@users.noreply.github.com>
2024-11-10 16:50:15 +0530

    Update and rename Permutations.java to _01_Permutations.java

--- Recursion/Recursion5/_01_Permutations.java
+++ Recursion/Recursion5/_01_Permutations.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Permutations {
     public static void main(String[] args) {
         permutation("abc");
+        System.out.println(permutationsCount("abc"));
     }
 
     static void permutation(String s){
@@ -35,5 +36,25 @@ public class Permutations {
             list.addAll(helper2(str,up.substring(1)));
         }
         return list;
+    }
+
+    /*
+        The permutation count can be deduced using formula= n!, But this program is written to understand the recursive flow
+        and the function calls of the program
+    */
+    static int permutationsCount(String s){
+        return permutationsCount("",s);
+    }
+    private static int permutationsCount(String p,String up){
+        if(up.isEmpty()){
+            return 1;
+        }
+        char c=up.charAt(0);
+        int count=0;
+        for(int i=0;i<=p.length();i++){
+            String str=p.substring(0,i)+c+p.substring(i);
+            count+=(permutationsCount(str,up.substring(1)));
+        }
+        return count;
     }
 }
